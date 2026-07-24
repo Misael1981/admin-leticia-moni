4a90b6581ada777fdd2ab4d82481b484e9b56ae5
Misael <misaelborges1981@gmail.com>
2026-07-24 11:31:26 -0300

    docs: ROADMAP

--- src/app/(admin)/dashboard/pacientes/[id]/components/WorkTabs/components/CardEvolutions/components/CardEvolutionForm/index.tsx
+++ src/app/(admin)/dashboard/pacientes/[id]/components/WorkTabs/components/CardEvolutions/components/CardEvolutionForm/index.tsx
@@ -165,6 +165,39 @@ const CardEvolutionForm = ({
             <FieldError>{errors.painScore?.message}</FieldError>
           </Field>
 
+          <div className="flex min-h-20 w-full items-center justify-center border p-4">
+            <div className="space-y-2 text-center">
+              <h3 className="font-heading text-foreground text-xl">
+                Aqui será um seletor para o status do tratamento
+              </h3>
+
+              <p>
+                Pra dizer se o paciente está em tratamento, em alta ou abandonou
+                o tratamento
+              </p>
+            </div>
+          </div>
+
+          <div className="flex min-h-20 w-full items-center justify-center border p-4">
+            <div className="space-y-2 text-center">
+              <h3 className="font-heading text-foreground text-xl">
+                Aqui o seletor de exercício (vídeos)
+              </h3>
+
+              <p>
+                Preciso que me passe se acha necessário enviar mais alguma
+                informação, tipo os espaçamento de tempo entre um exercício e
+                outro, o tempo que o paciente ( minutos ) que cada exercício
+                deve durar.
+              </p>
+              <p>
+                Assim que vc criar a sessão, os exercícios serão atualizados no
+                app do paciente e o celular receberá uma notificação, tipo
+                quando chega msn do whats...
+              </p>
+            </div>
+          </div>
+
           <Field>
             <FieldLabel>Notas</FieldLabel>
             <Controller
