e714b459c17052f6ee232f0a26dc44f6394fc72b
Misael <misaelborges1981@gmail.com>
2026-08-20 08:52:21 -0300

    fix: changing the matcher in the proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -40,5 +40,7 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|woff2?|ttf)$).*)",
+  ],
 }
